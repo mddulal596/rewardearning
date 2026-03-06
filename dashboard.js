f8e9df399fc88131ad3fcf7304b644bb8c138f45
MD DULAL HOSAN <42477795+mddulal596@users.noreply.github.com>
2026-03-06 23:46:27 +0600

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,7 +0,0 @@
-auth.onAuthStateChanged(user=>{
-    if(user){
-        db.ref("users/"+user.uid+"/balance").on("value",snap=>{
-            document.getElementById("balance").innerText = snap.val();
-        });
-    }
-});
