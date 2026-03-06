7b2c973a7f4994acc7d6b407e05c8122982a4231
MD DULAL HOSAN <42477795+mddulal596@users.noreply.github.com>
2026-03-06 23:08:22 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "reward-earning-d8165.firebaseapp.com",
-    databaseURL: "https://reward-earning-d8165-default-rtdb.firebaseio.com",
-    projectId: "reward-earning-d8165",
-    storageBucket: "reward-earning-d8165.appspot.com",
-    messagingSenderId: "175054692652",
-    appId: "1:175054692652:web:b26cc70cb0cc1aeb8e0c89"
+  apiKey: "AIzaSyA5cBiWpmWm8pDYoTO3i85zy7_J3XntkPY",
+  authDomain: "reward-earning-d8165.firebaseapp.com",
+  projectId: "reward-earning-d8165",
+  storageBucket: "reward-earning-d8165.firebasestorage.app",
+  messagingSenderId: "175054692652",
+  appId: "1:175054692652:web:b26cc70cb0cc1aeb8e0c89",
+  measurementId: "G-1LH3C41ZN3"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database();
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
