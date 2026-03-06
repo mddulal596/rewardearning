042d748ea54236408072ff6689566caa00334df4
MD DULAL HOSAN <42477795+mddulal596@users.noreply.github.com>
2026-03-07 00:03:30 +0600

    Delete tasks.js

--- tasks.js
+++
@@ -1,7 +0,0 @@
-function watchAd(){
-    let user = auth.currentUser;
-    setTimeout(()=>{
-        db.ref("users/"+user.uid+"/balance").transaction(b=>{return (b||0)+5});
-        alert("5 coins added");
-    },15000);
-}
