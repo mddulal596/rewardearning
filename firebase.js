b9b38fd8e286087a615bbad402e4c0ec8f139327
MD DULAL HOSAN <42477795+mddulal596@users.noreply.github.com>
2026-03-09 02:29:18 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -8,10 +7,8 @@ const firebaseConfig = {
   projectId: "reward-earning-d8165",
   storageBucket: "reward-earning-d8165.firebasestorage.app",
   messagingSenderId: "175054692652",
-  appId: "1:175054692652:web:b26cc70cb0cc1aeb8e0c89",
-  measurementId: "G-1LH3C41ZN3"
+  appId: "1:175054692652:web:b26cc70cb0cc1aeb8e0c89"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
